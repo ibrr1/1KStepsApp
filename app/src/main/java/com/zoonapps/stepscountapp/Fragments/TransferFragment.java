cca7dacee228cacf0aa98e863c4c53141eea3d8f
Ibrahim <ibrr1@hotmail.com>
2017-07-04 07:40:17 +0300

    back4app working

--- app/src/main/java/com/zoonapps/stepscountapp/Fragments/TransferFragment.java
+++ app/src/main/java/com/zoonapps/stepscountapp/Fragments/TransferFragment.java
@@ -9,8 +9,10 @@ import android.support.v4.app.Fragment;
 import android.text.SpannableStringBuilder;
 import android.text.style.ImageSpan;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -41,8 +43,9 @@ public class TransferFragment extends Fragment {
 
     int userCurrentSteps;
     double userCurrentEarning;
-    private RelativeLayout mTransLayout;
 
+    EditText mET1, mET2, mET3, mET4, mET5, mET6, mET7;
+    int mTransTypePosition;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -55,6 +58,14 @@ public class TransferFragment extends Fragment {
 //        mEmailTV = (TextView) rootView.findViewById(R.id.emailTV);
 //        mTotalStepsTV = (TextView) rootView.findViewById(R.id.totalStepsTV);
         mTotalEarning = (TextView) rootView.findViewById(R.id.totalEarningTV);
+        mET1 = (EditText) rootView.findViewById(R.id.et1);
+        mET2 = (EditText) rootView.findViewById(R.id.et2);
+        mET3 = (EditText) rootView.findViewById(R.id.et3);
+        mET4 = (EditText) rootView.findViewById(R.id.et4);
+        mET5 = (EditText) rootView.findViewById(R.id.et5);
+        mET6 = (EditText) rootView.findViewById(R.id.et6);
+        mET7 = (EditText) rootView.findViewById(R.id.et7);
+
         mProgressBar = (ProgressBar) rootView.findViewById(R.id.progressBar);
 
 
@@ -110,12 +121,114 @@ public class TransferFragment extends Fragment {
         ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter .createFromResource(getActivity(), R.array.trans_types, R.layout.spinner_center_item);
         mTransType.setAdapter(spinnerAdapter);
 
-        String transType= mTransType.getSelectedItem().toString();
 
 
-        // create editText
+        // get the selected payment type position
+//        mTransTypePosition = mTransType.getSelectedItemPosition();
+
+        mTransType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            public void onItemSelected(AdapterView<?> arg0, View view, int position, long id) {
+
+                if (mTransType.getSelectedItemPosition() == 0) {
+                    Toast.makeText(getActivity(), "الرجاء اختيار طريقة تحويل الرصيد!", Toast.LENGTH_SHORT).show();
+                    mET1.setVisibility(View.GONE);
+
+                } else if (mTransType.getSelectedItemPosition() == 1) {
+                    mET2.setVisibility(View.GONE);
+                    mET3.setVisibility(View.GONE);
+                    mET4.setVisibility(View.GONE);
+                    mET5.setVisibility(View.GONE);
+                    mET6.setVisibility(View.GONE);
+                    mET7.setVisibility(View.GONE);
+
+                    mET1.setVisibility(View.VISIBLE);
+                    mET1.setHint("البريد الالكتروني او الجوال لدى باي بال");
+
+                } else if (mTransType.getSelectedItemPosition() == 2) {
+                    mET5.setVisibility(View.GONE);
+                    mET6.setVisibility(View.GONE);
+                    mET7.setVisibility(View.GONE);
+
+                    mET1.setVisibility(View.VISIBLE);
+                    mET2.setVisibility(View.VISIBLE);
+                    mET3.setVisibility(View.VISIBLE);
+                    mET4.setVisibility(View.VISIBLE);
+
+                    mET1.setHint("الاسم الثلاثي");
+                    mET2.setHint("اسم البنك");
+                    mET3.setHint("رقم الايبان");
+                    mET4.setHint("تأكيد رقم الايبان");
+
+                }  else if (mTransType.getSelectedItemPosition() == 3){
+                    mET1.setVisibility(View.VISIBLE);
+                    mET2.setVisibility(View.VISIBLE);
+                    mET3.setVisibility(View.VISIBLE);
+                    mET4.setVisibility(View.VISIBLE);
+                    mET5.setVisibility(View.VISIBLE);
+                    mET6.setVisibility(View.VISIBLE);
+                    mET7.setVisibility(View.VISIBLE);
+
+                    mET1.setHint("الاسم الكامل");
+                    mET2.setHint("الدولة");
+                    mET3.setHint("المدينة");
+                    mET4.setHint("اسم البنك");
+                    mET5.setHint("عنوان البنك");
+                    mET5.setHint("فرع البنك");
+                    mET6.setHint("رقم الايبان");
+                    mET7.setHint("تأكيد رقم الايبان");
+                }
+            }
+            public void onNothingSelected(AdapterView<?> arg0) { }
+        });
 
 
+
+
+//        mTransType.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                if (mTransTypePosition == 0){
+//                    Toast.makeText(getActivity(), "الرجاء اختيار طريقة تحويل الرصيد!", Toast.LENGTH_SHORT).show();
+//
+//                }
+//
+//                else if(mTransTypePosition == 1){
+//                    mET1.setVisibility(View.VISIBLE);
+//                    mET1.setHint("البريد الالكتروني او الجوال لدى باي بال");
+//
+//                }
+//                else if (mTransTypePosition == 2){
+//                    mET1.setVisibility(View.VISIBLE);
+//                    mET2.setVisibility(View.VISIBLE);
+//                    mET3.setVisibility(View.VISIBLE);
+//                    mET4.setVisibility(View.VISIBLE);
+//
+//                    mET1.setHint("الاسم الثلاثي");
+//                    mET2.setHint("اسم البنك");
+//                    mET3.setHint("رقم الايبان");
+//                    mET4.setHint("تأكيد رقم الايبان");
+//                } else if (mTransTypePosition == 3){
+//                    mET1.setVisibility(View.VISIBLE);
+//                    mET2.setVisibility(View.VISIBLE);
+//                    mET3.setVisibility(View.VISIBLE);
+//                    mET4.setVisibility(View.VISIBLE);
+//                    mET5.setVisibility(View.VISIBLE);
+//                    mET6.setVisibility(View.VISIBLE);
+//                    mET7.setVisibility(View.VISIBLE);
+//
+//                    mET1.setHint("الاسم الكامل");
+//                    mET2.setHint("الدولة");
+//                    mET3.setHint("المدينة");
+//                    mET4.setHint("اسم البنك");
+//                    mET5.setHint("عنوان البنك");
+//                    mET5.setHint("فرع البنك");
+//                    mET6.setHint("رقم الايبان");
+//                    mET7.setHint("تأكيد رقم الايبان");
+//                }
+//            }
+//        });
 
         return rootView;
 
